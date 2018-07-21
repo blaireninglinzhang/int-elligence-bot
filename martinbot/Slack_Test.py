c57a9d096b889dc4a8e62f390f36bc1d0a515856
martin-leung <martikl1@uci.edu>
2018-07-21 16:13:08 -0700

    timing implementation

--- martinbot/Slack_Test.py
+++ martinbot/Slack_Test.py
@@ -3,11 +3,12 @@ import json
 import time
 import re
 from slackclient import SlackClient
-
+import datetime as dt
+from time import sleep
 
 # instantiate Slack client
-slack_client = SlackClient('xoxp-369235392373-394289804691-402167874849-96954213365860c4e8fe67dcd36b7d91')
-bot_client = SlackClient('xoxb-369235392373-404015005318-bECdwvQkRTYlhY4S5D6jM2DL')
+slack_client = SlackClient('xoxp-369235392373-394289804691-402487589652-3dc2752ebb24f0fda4f22a7b9393196e')
+bot_client = SlackClient('xoxb-369235392373-404015005318-KPVfTScYQHbF47do1kz68wLG')
 # starterbot's user ID in Slack: value is assigned after the bot starts up
 starterbot_id = None
 
@@ -41,24 +42,44 @@ def parse_direct_mention(message_text):
         and returns the user ID which was mentioned. If there is no direct mention, returns None
     """
     matches = re.search(MENTION_REGEX, message_text)
-    # the first group contains the username, the second group contains the remaining message
     return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
 
 
 def handle_command(command, channel):
-    """
-        Executes bot command if the command is known
-    """
-    # Default response is help text for the user
+    
     default_response = "Not sure what you mean. Try {0}.".format(DO_COMMAND)
 
-    # Finds and executes the given command, filling in response
     response = None
 
     # This is where you start to implement more commands!
     if command.startswith(DO_COMMAND):
-        response = "Tracking History"
+        #response = "/remind #int-elligence hihi in 1 minute"
+        latest_post()
 
+           
+    elif command.startswith(HELLO_COMMAND):
+        response = "Hi everyone! I'm a bot, bleep, bloop."
+
+    # Sends the response back to the channel
+    slack_client.api_call(
+        "chat.postMessage",
+        channel=channel,
+        text=response or default_response
+    )
+
+def latest_post():
+    history = slack_client.api_call(
+                "channels.history",
+                 channel = channel_code,
+                              )
+    milestone = history["messages"][1]
+    print(milestone)
+
+def counting():
+
+        print("counting")
+        
+        hot = []
         
         history = slack_client.api_call(
                             "channels.history",
@@ -71,7 +92,7 @@ def handle_command(command, channel):
         timestamp = 0 #testing
         
         count = 0
-        print(history_list[0])
+        #print(history_list[0])
         for i in history_list:
             reaction_index = 0
             reaction_count = 0
@@ -80,12 +101,16 @@ def handle_command(command, channel):
                 for j in history_list[count]["reactions"]:
                     reaction_count += history_list[count]["reactions"][reaction_index]["count"]
                     reaction_index += 1
+                    if reaction_index > 2:
+                        #print("hot")
+                        hot.append(history_list[count])
             #print(reaction_count)
             count += 1
             
+
         timestamp = history["messages"][0]["ts"]
       
-        print("timestamp: " + timestamp)
+        #print("timestamp: " + timestamp)
 
         
         reactions = slack_client.api_call(
@@ -95,23 +120,31 @@ def handle_command(command, channel):
             timestamp= timestamp
             )
 
-        print(reactions)              
-    elif command.startswith(HELLO_COMMAND):
-        response = "Hi everyone! I'm a bot, bleep, bloop."
+        #print(reactions)
 
-    # Sends the response back to the channel
-    slack_client.api_call(
-        "chat.postMessage",
-        channel=channel,
-        text=response or default_response
-    )
-
+def member_join():
+    join = slack_client.api_call(
+       "member_joined_channel")
+    #users = slack_client.api_call(
+        #"channels.list")
+    #print(users)
+    print(join)
 
 if __name__ == "__main__":
     if bot_client.rtm_connect(with_team_state=False):
         print("Starter Bot connected and running!")
 
-        # Read bot's user ID by calling Web API method `auth.test`
+        t = dt.datetime.now()
+        minute_count = 0 
+        while True:
+            delta = dt.datetime.now()-t              
+            if delta.seconds >= 300:
+                counting()
+                #slack_client.api_call("chat.postMessage", channel = channel_code, text = "HOT(lots of emojis)" )
+                #slack_client.api_call("chat.postMessage", channel = channel_code, text = "this event happended 10 seconds ago!!!" + str(i['text']))
+                t = dt.datetime.now()
+        #member_join()
+                
         starterbot_id = bot_client.api_call("auth.test")["user_id"]
         while True:
             command, channel = parse_bot_commands(bot_client.rtm_read())
